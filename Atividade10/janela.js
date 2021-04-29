d3c41c72ea109016f7fc9af4e5cee22f8e18d7e5
GustaRusso <66815347+GustaRusso@users.noreply.github.com>
2021-04-28 21:54:00 -0300

    Add files via upload

---
+++ Atividade10/janela.js
@@ -0,0 +1,12 @@
+function mDown(obj) {
+	document.getElementById("janela").src ="janelaquebra.png";
+}
+
+function mMover() {
+	document.getElementById("janela").src ="janelaaberta.png";
+}
+
+
+function mOut(obj) {
+	document.getElementById("janela").src ="janelafechada.png";
+}
