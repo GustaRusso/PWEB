fd3079e9580155849347e17062e8933096726d93
GustaRusso <66815347+GustaRusso@users.noreply.github.com>
2021-05-02 23:23:51 -0300

    Add files via upload

---
+++ portfolio/js/funcoes.js
@@ -0,0 +1,23 @@
+function educacao()
+{
+	document.getElementById("informacoes").innerHTML = "<p>Técnico em mecatrônica Etec Rubens de Faria e Sousa<\p>" + 
+											 "<p>Jan/2016 Dez/2018 (concluído)<br><\p><br>" + 
+											 "<p>Superior em Análise e Desenvolviment o de Sistemas Faculdade de Tecnologia José Crespo Gonzales<\p>" + 
+											 "<p>Jan/2019 Dez/2022 (cursando o quinto semestre)</p><br>" +
+											 "<p>Inglês Intermediario<\p>";
+}
+function sobreMim()
+{
+	document.getElementById("informacoes").innerHTML = "<p><b>Nome Completo:</b> Gustavo Russo Santos<\p>" + 
+											 "<p><b>Idade:</b> 19<\p>" + 
+											 "<p><b>Profissão atual:</b> Suporte Técnico<\p>";
+}
+function conhecimentos()
+{
+	document.getElementById("informacoes").innerHTML = "<ul><li><p>Linguagem C<\p></li> " + 
+											 "<li><p>Java<\p></li>" +  
+											 "<li><p>Oracle SQL<\p></li>" + 
+											 "<li><p>HTML5<\p></li>" + 
+											 "<li><p>CSS3<\p></li>" + 
+											 "<li><p>JavaScript<\p></li></ul>";
+}
